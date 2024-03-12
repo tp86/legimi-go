07b777a90cfe59aeef9483b962d34921e0201a99
tp86 <tom0palka@gmail.com>
2024-03-12 09:45:02 +0100

    Numbers with length encoded

--- internal/byteconverter/byteconverter.go
+++ internal/byteconverter/byteconverter.go
@@ -44,6 +44,37 @@ type (
 	RawLong  = number[uint64]
 )
 
+type lengths interface {
+	uint16 | uint32
+}
+
+type numberWithLength[N numbers, L lengths] struct {
+	Value N
+}
+
+func (nl *numberWithLength[N, L]) writeBytesTo(w io.ByteWriter) {
+	l := number[L]{L(reflect.TypeOf(nl.Value).Size())}
+	l.writeBytesTo(w)
+	n := number[N]{nl.Value}
+	n.writeBytesTo(w)
+}
+
+func (nl *numberWithLength[N, L]) readBytesFrom(r io.ByteReader) {
+	for i := uintptr(0); i < reflect.TypeOf(L(0)).Size(); i++ {
+		r.ReadByte()
+	}
+	n := number[N]{}
+	n.readBytesFrom(r)
+	nl.Value = n.Value
+}
+
+type (
+	Byte  = numberWithLength[uint8, uint32]
+	Short = numberWithLength[uint16, uint32]
+	Int   = numberWithLength[uint32, uint32]
+	Long  = numberWithLength[uint64, uint32]
+)
+
 func WriteBytesTo(w io.ByteWriter, b bytesWriterTo) {
 	b.writeBytesTo(w)
 }
--- internal/byteconverter/byteconverter_test.go
+++ internal/byteconverter/byteconverter_test.go
@@ -14,6 +14,10 @@ var writingTests = map[string]struct {
 	"raw short": {&RawShort{6}, []byte{6, 0}},
 	"raw int":   {&RawInt{16386}, []byte{0x02, 0x40, 0, 0}},
 	"raw long":  {&RawLong{9223372036854775807}, []byte{255, 255, 255, 255, 255, 255, 255, 0x7f}},
+	"byte":      {&Byte{17}, []byte{1, 0, 0, 0, 0x11}},
+	"short":     {&Short{4}, []byte{2, 0, 0, 0, 4, 0}},
+	"int":       {&Int{24}, []byte{4, 0, 0, 0, 0x18, 0, 0, 0}},
+	"long":      {&Long{8}, []byte{8, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0}},
 }
 
 func TestWritingBytes(t *testing.T) {
@@ -24,7 +28,7 @@ func TestWritingBytes(t *testing.T) {
 			WriteBytesTo(buf, tc.input)
 			bs := buf.Bytes()
 			if !slices.Equal(bs, tc.expected) {
-				t.Errorf("%s: expectedn%v, got %v", name, tc.expected, bs)
+				t.Errorf("%s: expected %v, got %v", name, tc.expected, bs)
 			}
 		})
 	}
@@ -39,6 +43,10 @@ func (n number[N]) value() any {
 	return n.Value
 }
 
+func (nl numberWithLength[N, L]) value() any {
+	return nl.Value
+}
+
 var readingTests = map[string]struct {
 	input    []byte
 	output   value
@@ -48,6 +56,10 @@ var readingTests = map[string]struct {
 	"raw short": {[]byte{6, 0}, &RawShort{}, uint16(6)},
 	"raw int":   {[]byte{0x02, 0x40, 0, 0}, &RawInt{}, uint32(16386)},
 	"raw long":  {[]byte{255, 255, 255, 255, 255, 255, 255, 0x7f}, &RawLong{}, uint64(9223372036854775807)},
+	"byte":      {[]byte{1, 0, 0, 0, 1}, &Byte{}, byte(1)},
+	"short":     {[]byte{2, 0, 0, 0, 3, 0}, &Short{}, uint16(3)},
+	"int":       {[]byte{4, 0, 0, 0, 0x02, 0x40, 0, 0}, &Int{}, uint32(16386)},
+	"long":      {[]byte{8, 0, 0, 0, 255, 255, 255, 255, 255, 255, 255, 0x7f}, &Long{}, uint64(9223372036854775807)},
 }
 
 func TestReadingBytes(t *testing.T) {
