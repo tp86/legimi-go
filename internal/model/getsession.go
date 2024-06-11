1580210768d43f8e4bc94b0bada491de8fa8658c
tp86 <tom0palka+tp86@gmail.com>
2024-06-11 10:15:41 +0200

    Debug session response

--- internal/model/getsession.go
+++ internal/model/getsession.go
@@ -45,11 +45,39 @@ func (s GetSession) asMap() encoding.Map {
 }
 
 type Session struct {
-	Id string
+	state               uint16
+	errorCode           uint16
+	lastAgreementPeriod bool
+	serverMessage       string
+	maxTablets          uint32
+	maxSmartphones      uint32
+	maxEinks            uint32
+	maxWins             uint32
+	maxKindles          uint32
+	maxDownloads        uint32
+	downloadsLeft       uint32
+	activating          bool
+	maxDevices          uint32
+	Id                  string
 }
 
 func (s *Session) Decode(r io.Reader) (int, error) {
-	return encoding.Decode(r, encoding.Map{7: &s.Id})
+	return encoding.Decode(r, encoding.Map{
+		0:  &s.state,
+		5:  &s.errorCode,
+		37: &s.lastAgreementPeriod,
+		3:  &s.serverMessage,
+		15: &s.maxTablets,
+		14: &s.maxSmartphones,
+		16: &s.maxEinks,
+		17: &s.maxWins,
+		32: &s.maxKindles,
+		33: &s.maxDownloads,
+		34: &s.downloadsLeft,
+		21: &s.activating,
+		27: &s.maxDevices,
+		7:  &s.Id,
+	})
 }
 
 func (s Session) Type() uint16 {
@@ -59,5 +87,18 @@ func (s Session) Type() uint16 {
 func (s Session) DebugFormat() string {
 	var b strings.Builder
 	b.WriteString("Session response:\n")
+	b.WriteString(fmt.Sprintf("state: %d\n", s.state))
+	b.WriteString(fmt.Sprintf("error code: %d\n", s.errorCode))
+	b.WriteString(fmt.Sprintf("in last agreement period: %t\n", s.lastAgreementPeriod))
+	b.WriteString(fmt.Sprintf("is activating: %t\n", s.activating))
+	b.WriteString(fmt.Sprintf(`server message: "%s"`+"\n", s.serverMessage))
+	b.WriteString(fmt.Sprintf("max tablets: %d\n", s.maxTablets))
+	b.WriteString(fmt.Sprintf("max smartphones: %d\n", s.maxSmartphones))
+	b.WriteString(fmt.Sprintf("max einks: %d\n", s.maxEinks))
+	b.WriteString(fmt.Sprintf("max windows: %d\n", s.maxWins))
+	b.WriteString(fmt.Sprintf("max kindles: %d\n", s.maxKindles))
+	b.WriteString(fmt.Sprintf("max devices: %d\n", s.maxDevices))
+	b.WriteString(fmt.Sprintf("max downloads: %d\n", s.maxDownloads))
+	b.WriteString(fmt.Sprintf("downloads left: %d", s.downloadsLeft))
 	return b.String()
 }
